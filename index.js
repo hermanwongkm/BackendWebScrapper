29261b0998749ee9ed81a0d1c95fcf3e4401fb11
Soong Cun Yuan <zengersoong@gmail.com>
2019-12-11 15:12:28 +0800

    added empty table strategy

--- index.js
+++ index.js
@@ -26,10 +26,22 @@ async function getBody(parameter) {
 
 extractTable = body => {
   {
+    //[0.0] flag represents the state of the automation process.
+    flag = 0;
     //loading html body to cheerio
     var $ = cheerio.load(body);
     cheerioTableparser($);
     var data = $("table.restable").parsetable(true, true, true);
+    // if the search term returns an empty table
+    if(typeof data == "undefined" || data == null || data.length == null
+    || data.length == 0){
+      data = [
+        ["","","","","","","EMPTY",],
+        ["","","","","","","EMPTY",],
+        ["","","","","","","EMPTY",]
+      ];
+      flag = 1;
+    };
     var heading = data[0];
     var suffix = data[1];
     var description = data[2];
@@ -62,7 +74,7 @@ extractTable = body => {
         description: description[i]
       });
     }
-    return { results, csv };
+    return { results, csv, flag};
   }
 };
 
@@ -70,71 +82,6 @@ app.get("/test/:search", async function(req, res) {
   console.log(req.params.search);
   let query = req.params.search;
   var body = await getBody(query);
-  const { results, csv } = extractTable(body); //Object deconstruction
-  res.send([results, csv]);
+  const { results, csv ,flag  } = extractTable(body); //Object deconstruction
+  res.send([results, csv, flag]);
 });
-
-// const cheerio = require("cheerio");
-// const Nightmare = require("nightmare");
-// const nightmare = Nightmare({ show: false });
-// cheerioTableparser = require("cheerio-tableparser");
-// var cors = require("cors");
-
-// var express = require("express");
-// var app = express();
-// app.use(cors());
-// const port = 3001;
-// app.listen(port, () =>
-//   console.log("The server is currently listening on: " + port)
-// );
-
-// function getInfo(parameter, res) {
-//   nightmare
-//     .goto("https://hts.usitc.gov/?query=" + parameter)
-//     .wait(5000)
-//     .evaluate(function() {
-//       //here is where I want to return the html body
-//       return document.body.innerHTML;
-//     })
-//     .then(function(body) {
-//       //loading html body to cheerio
-//       var $ = cheerio.load(body);
-//       cheerioTableparser($);
-//       var data = $("table.restable").parsetable(true, true, true);
-//       var heading = data[0];
-//       var suffix = data[1];
-//       var description = data[2];
-//       // the start of the table is 6
-//       // console.log(heading[6],suffix[6],description[6]);
-//       var i;
-//       var results = [];
-
-//       //[0.1] Replace empty string with parent or default "00" for suffix
-//       let previous = "ERR";
-//       for (i = 6; i < data[0].length; i++) {
-//         if (suffix[i] == "") {
-//           suffix[i] = "00";
-//         }
-
-//         if (heading[i] != "") {
-//           previous = heading[i];
-//         } else {
-//           heading[i] = previous;
-//         }
-//         //[0.1] End
-//         results.push({
-//           data: heading[i],
-//           suffix: suffix[i],
-//           description: description[i]
-//         });
-//       }
-//       res.send(results);
-//       return;
-//     });
-// }
-
-// app.get("/test/:search", function(req, res) {
-//   console.log(req.params.search);
-//   let query = req.params.search;
-//   var results = getInfo(query, res);
-// });
